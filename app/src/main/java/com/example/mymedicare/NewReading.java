8a64d77240c1ecad9e6e7649f9f90540291ee0df
SomeGuyEatingPie <seandavidjohnson132@gmail.com>
2023-04-04 15:26:58 +0100

    More Activities 2: Electric Booglaloo

--- app/src/main/java/com/example/mymedicare/NewReading.java
+++ app/src/main/java/com/example/mymedicare/NewReading.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class new_reading extends AppCompatActivity {
+public class NewReading extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
