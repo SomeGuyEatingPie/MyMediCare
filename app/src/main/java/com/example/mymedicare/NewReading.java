380f9f9d06390eebee652d1d9b93b71a1da06f6b
SomeGuyEatingPie <seandavidjohnson132@gmail.com>
2023-04-04 15:24:33 +0100

    More activities

---
+++ app/src/main/java/com/example/mymedicare/NewReading.java
@@ -0,0 +1,14 @@
+package com.example.mymedicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_reading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_reading);
+    }
+}
